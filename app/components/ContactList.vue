a79276ac7eb1a2fdef1fb755834954fd3018055f
Vinicius Teixeira <vinicius0026@gmail.com>
2018-01-07 21:23:12 -0200

    added search in contacts list

--- app/components/ContactList.vue
+++ app/components/ContactList.vue
@@ -4,9 +4,24 @@
     sm6
     md4
   >
+    <v-container>
+      <v-layout class="search-box pa-1">
+        <v-flex xs1>
+          <v-icon>search</v-icon>
+        </v-flex>
+        <v-flex xs11>
+          <input
+            type="text"
+            class="search-box-input"
+            @keyup="filter"
+            v-model="searchTerm"
+          >
+        </v-flex>
+      </v-layout>
+    </v-container>
     <v-list>
       <v-list-tile
-        v-for="contact in contacts"
+        v-for="contact in filteredContacts"
         :key="contact.id"
         @click="selectContact(contact.id)"
       >
@@ -22,12 +37,40 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import { debounce } from 'lodash'
 export default {
+  data () {
+    return {
+      searchTerm: '',
+      filteredContacts: []
+    }
+  },
   computed: {
     ...mapGetters(['contacts'])
   },
+  mounted () {
+    this.filteredContacts = this.contacts
+  },
   methods: {
-    ...mapActions(['selectContact'])
+    ...mapActions(['selectContact']),
+    filter: debounce(function () {
+      const filterRegex = new RegExp(this.searchTerm, 'i')
+      this.filteredContacts = this.contacts.filter(contact => contact.fullName.match(filterRegex))
+    }, 100)
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.search-box
+  border 1px solid rgba(0,0,0,.25)
+  border-radius 2px
+
+  &-input
+    width 100%
+    height 100%
+    font-family 'Roboto', sans-serif
+
+    &:focus
+      outline none
+</style>
